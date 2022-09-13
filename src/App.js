afc139de75c0bf5b2f1411278ad94298b668e116
bipularyal <59837845+bipularyal@users.noreply.github.com>
2022-09-12 23:08:02 -0500

    html work finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const digit = []
+    const createDigits = () => {
+      for (let index = 0; index < 10; index++) {
+        digit[index] = index+1
+      }
+    }
+    createDigits()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="fi-calc">
+        <div className="fi-disp">
+            <span>(0)</span> 0
+        </div>
+        <div className="fi-oprator">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+          <div className="fi-digit">
+            {digit.map((val) => <button> {val}</button>)}
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+
+          </div>
+     </div>
     </div>
   );
 }
