d879f7bb04a55984eb4bbf86c0a59f9488457f80
bipularyal <59837845+bipularyal@users.noreply.github.com>
2022-09-13 14:30:04 -0500

    Completed the calculator app fully

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [calc,setCalc] = useState("")
+const [result,setResult] = useState("")
+
   const digit = []
     const createDigits = () => {
       for (let index = 0; index < 9; index++) {
@@ -9,29 +13,53 @@ function App() {
       }
     }
     createDigits()
+
+    const calculate = () => {
+      setCalc(eval(calc).toString());
+    }
+    const deleteLast = () => {
+      if (calc == ""){
+        return
+      }
+      const value = calc.slice(0,-1)
+      setCalc(value)
+    }
+    const ops = ['/','*','+','-', '.']
+    const updateCalc = value => {
+      if (
+        ops.includes(value) && calc === "" || 
+        ops.includes(value) && ops.includes(calc.slice(-1)))
+      {
+        return;
+      }
+      setCalc(calc+value)
+      if (!ops.includes(value)){
+        setResult(eval(calc+value).toString());
+      }
+    }
   return (
     <div className="App">
      <div className="fi-calc">
         <div className="fi-disp">
-            <span>(0)0</span>
+            {result ? <span>({result}) </span> : " "}
+            {calc || " 0"}
         </div>
         <div className="fi-oprators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
-          <button>DEL</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={deleteLast}>DEL</button>
         </div>
           <div className="fi-digits">
-            {digit.map((val) => <button> {val}</button>)}
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
-
+            {digit.map((val) => <button onClick={ () => updateCalc(val.toString())} key = {val}> {val}</button>)}
+            <button onClick={() => updateCalc("0")} key= {0}>0</button>
+            <button onClick={() => updateCalc(".")} key = {11}>.</button>
+            <button onClick = {calculate}>=</button>
+ 
           </div>
      </div>
     </div>
   );
 }
-
 export default App;
