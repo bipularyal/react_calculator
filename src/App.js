b4800ea90c0ea5a98129a1d835ddc0bac8d9397c
bipularyal <59837845+bipularyal@users.noreply.github.com>
2022-09-12 23:57:34 -0500

    CSS added to the calculator app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const digit = []
     const createDigits = () => {
-      for (let index = 0; index < 10; index++) {
+      for (let index = 0; index < 9; index++) {
         digit[index] = index+1
       }
     }
@@ -13,16 +13,16 @@ function App() {
     <div className="App">
      <div className="fi-calc">
         <div className="fi-disp">
-            <span>(0)</span> 0
+            <span>(0)0</span>
         </div>
-        <div className="fi-oprator">
+        <div className="fi-oprators">
           <button>/</button>
           <button>*</button>
           <button>+</button>
           <button>-</button>
           <button>DEL</button>
         </div>
-          <div className="fi-digit">
+          <div className="fi-digits">
             {digit.map((val) => <button> {val}</button>)}
             <button>0</button>
             <button>.</button>
